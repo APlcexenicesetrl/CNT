00dd681fe70fdaa7ea40e410b674fc6eba0ebac5
taim_way@163.com <taim_way@163.com.com>
2024-06-15 16:05:27 +0800

    Information Upload

---
+++ config-server.js
@@ -0,0 +1,10 @@
+class information{
+    constructor(){
+        "product":{
+           "version":"1.1.5.49",
+            "Status":"Active",
+            "license":"MIT",
+            "Developers":["TaimWay"] 
+        }
+    }
+}
