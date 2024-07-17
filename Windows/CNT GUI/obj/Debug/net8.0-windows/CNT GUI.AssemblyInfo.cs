d736804968c3f8deabc37a0f977a2c1e4dc417dd
TaimWay <taim_way@163.com>
2024-07-17 13:23:28 +0800

    CNT GUI Update

--- Windows/CNT GUI/obj/Debug/net8.0-windows/CNT GUI.AssemblyInfo.cs
+++ Windows/CNT GUI/obj/Debug/net8.0-windows/CNT GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNT GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485d52e1a038dd26054a39f9df2d7ae36314ecd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07420056329815af32a98a14137a918f66849e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNT GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNT GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
